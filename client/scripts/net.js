e7da7556904eef6243fc4edc6672ec48404f6551
TdroL <michal12.mail@gmail.com>
2010-11-03 12:06:21 +0100

    prototype v0.3.1 - net tweaks

--- client/scripts/net.js
+++ client/scripts/net.js
@@ -1,19 +1,36 @@
 (function($) {
 	net = {
-		host: '192.168.1.100:8000',
-		connection: null,
+		ws: null,
 		id: 0,
 		error: false,
+		_data: {},
 		init: function() {
 			if( ! ('WebSocket' in window))
 			{
 				return;
 			}
 			
-			ws = new WebSocket('ws://'+this.host);
+			this.ws = ws = new WebSocket('ws://'+(/^https?:\/\/([^\/]+)\//.exec(window.location)[1])+':8000');
 			
-			ws.onmessage = function(message) {
+			ws.on = function(event,  callback) {
+				ws['on'+event] = callback;
+			};
+			
+			ws.on('message', function(message) {
 				var data = message.data, result;
+				
+				if((result = /^update:(\{.+\})$/.exec(data)))
+				{
+					data = JSON.parse(result[1]);			
+					
+					if('id' in data && data.id && data.id != net.id)
+					{
+						player.opponents[data.id] = player.opponents[data.id] || {x: 15, y: 15};
+						player.opponents[data.id].x = data.x;
+						player.opponents[data.id].y = data.y;
+					}
+					return;
+				}
 				
 				if((result = /^id:(\d+)$/.exec(data)))
 				{
@@ -29,29 +46,21 @@
 					}
 					return;
 				}
-				
-				if((result = /^update:(\{.+\})$/.exec(data)))
-				{
-					data = JSON.parse(result[1]);			
-					
-					if('id' in data && data.id && data.id != net.id)
-					{
-						player.opponents[data.id] = player.opponents[data.id] || {x: 15, y: 15};
-						player.opponents[data.id].x = data.x;
-						player.opponents[data.id].y = data.y;
-					}
-					return;
-				}
-			};
+			});
 			
-			window.setInterval(function() {
-				ws.send('update:'+JSON.stringify({
-					id: net.id,
-					date: +new Date(),
-					x: Math.round2(player.ball.x),
-					y: Math.round2(player.ball.y)
-				}));
-			}, 20); // ms
+			ws.on('open', function() {
+				window.setInterval(function() {
+					if(ws.bufferedAmount == 0)
+					{
+						net._data.id = net.id;
+						net._data.date = +new Date();
+						net._data.x = Math.round2(player.ball.x);
+						net._data.y = Math.round2(player.ball.y);
+						
+						ws.send('update:'+JSON.stringify(net._data));
+					}
+				}, 20); // ms
+			});
 		}
 	};
 
